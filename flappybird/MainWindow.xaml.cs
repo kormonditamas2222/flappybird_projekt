0422e88873d4e24921012eacb138dd15abef8c4f
Petrik Diák <diak@petrik.hu>
2026-01-07 09:28:45 +0100

    aha

--- flappybird/MainWindow.xaml.cs
+++ flappybird/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace flappybird
 {
@@ -19,6 +20,22 @@ namespace flappybird
         public MainWindow()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+            timer.Tick += Timer_Tick;
         }
-    }
+		int speed = 1;
+		private void Timer_Tick(object sender, EventArgs e)
+        {
+           
+        }
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+            sp.Visibility = Visibility.Hidden;
+            sajd.Visibility = Visibility.Visible;
+            hatter.Visibility = Visibility.Visible;
+		}
+	}
 }
